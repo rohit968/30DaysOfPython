5526cf1c56e9777824d2024e040c696e0aff1017
Rohit Tiwari <rohit.tiwari968@gmail.com>
2026-03-27 01:12:21 +0530

    Day 1 Exercise

---
+++ Day 1.py
@@ -0,0 +1,29 @@
+# DAY - 1 Exercises
+
+# Checking Python version on PowerShell
+#python -- version
+
+# Mathematical operations
+print (3 + 4)
+print (3 - 4)
+print (3 * 4)
+print (3 % 4)
+print (3 / 4)
+print (3 ** 4)
+print (3 // 4)
+
+# Strings
+print("Rohit Tiwari")
+print ("Rohit Tiwari")
+print ("India")
+print ("I am enjoying 30 days of python")
+
+# Checking Data Types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Rohit Tiwari'))
+print(type('Rohit Tiwari'))
+print(type("India"))
