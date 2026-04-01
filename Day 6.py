a0fa4a9d07d82b2cf91b8e31fa4c6977c883e1a6
Rohit Tiwari <rohit.tiwari968@gmail.com>
2026-04-01 14:57:59 +0530

    Complete Day 6
    Both exercises of day 6 completed

--- Day 6.py
+++ Day 6.py
@@ -19,3 +19,31 @@ print(len(siblings))
 family_members = siblings + ('Raj', 'Rekha')
 
 
+# Exercise Level 2
+
+#1
+family, *siblings = family_members[-2:], family_members[:7]
+
+#2
+fruits = ('bananna', 'apple', 'kiwi')
+vegetables = ('ladyfinger', 'tomato', 'potato')
+animal_products = ('urea', 'fodder')
+food_stuff_up = fruits + animal_products + vegetables
+
+#3
+food_stuff_lt = list(food_stuff_up)
+
+#4
+print(food_stuff_lt[len(food_stuff_lt)//2])
+
+#5
+print('First three items: ', food_stuff_lt[:3])
+print('Last three items: ', food_stuff_lt[-3:])
+
+#6
+del food_stuff_up
+
+#7
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)
+print('Iceland' in nordic_countries)
