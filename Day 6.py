ca2e9b1045e36f20ffd1f81d51e4b58dc99270f2
Rohit Tiwari <rohit.tiwari968@gmail.com>
2026-04-01 14:54:49 +0530

    Day 6 Exercise 1
    Exercise 1 of Day 6 completed

---
+++ Day 6.py
@@ -0,0 +1,21 @@
+c
+
+# Exercise Level 1
+
+# 1
+empty_tuple = ()
+
+#2
+brothers = ('Aditya', 'Rohit', 'Saurabh', 'Nikhil')
+sisters = ('Shikhs', 'Deepa', 'Shreya')
+
+#3
+siblings = brothers + sisters
+
+#4
+print(len(siblings))
+
+#5
+family_members = siblings + ('Raj', 'Rekha')
+
+
