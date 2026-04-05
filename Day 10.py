503ad2975cf388099a81858d962a89813be62e5e
Rohit Tiwari <rohit.tiwari968@gmail.com>
2026-04-05 19:44:33 +0530

    Day 10 - Exercise 1
    Exercise 1 of Day 10 completed

--- Day 10.py
+++ Day 10.py
@@ -1,1 +1,53 @@
 # DAY 10: 30 Days of python programming
+
+#Exercise Level 1
+
+#1
+for i in range(10):
+    print(i)
+
+a = 0
+while a < 10:
+    print(a)
+    a += 1
+
+#2
+for i in range(10, 0, -1):
+    print(i)
+
+a = 10
+while a > 0:
+    print(a)
+    a -= 1
+
+#3
+for i in range(1, 8):
+    print('#' * i)
+
+#4
+for i in range(1, 8):
+    for j in range(1, 8):
+        print('#', end=' ')
+    print('\n')
+
+#5
+i = 0
+while i < 11:
+    print('{} * {} = {}'.format(i, i, i * i))
+    i += 1
+
+#6
+tech_list = ['Python', 'Numpy','Pandas','Django', 'Flask']
+for item in tech_list:
+    print(item)
+
+#7
+for i in range (101):
+    if i % 2 == 0:
+        print(i)
+
+#8
+for i in range (101):
+    if i % 2 != 0:
+        print(i)
+
