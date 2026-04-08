4c56b1025548996686431735d96f84b540ea79c6
Rohit Tiwari <rohit.tiwari968@gmail.com>
2026-04-09 01:50:24 +0530

    Complete Day 13
    All exercises of day 13 completed

--- Day 13.py
+++ Day 13.py
@@ -1,3 +1,18 @@
 # DAY 13: 30 Days of python programming
 
-#Exercise 
+#Exercise
+
+#1
+numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+positive_list = [i for i in numbers if i >= 0]
+print(positive_list)
+
+#2
+list_of_lists =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flaten_list = [item for sub_item in list_of_lists for item in sub_item ]
+print(flaten_list)
+
+#3
+names = [[('Asabeneh', 'Yetayeh')], [('David', 'Smith')], [('Donald', 'Trump')], [('Bill', 'Gates')]]
+new_list = [' '.join(item) for sub_item in names for item in sub_item]
+print(new_list)
