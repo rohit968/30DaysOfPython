95d08b4c259650dddcfc90c6f6a7bdd40350b21f
Rohit Tiwari <rohit.tiwari968@gmail.com>
2026-04-10 22:37:47 +0530

    Exercise 1 completed
    Exercise Level 1 of day 14 completed

--- Day 14.py
+++ Day 14.py
@@ -1,1 +1,27 @@
 # DAY 14: 30 Days of python programming
+
+# Exercise Level 1
+
+#1 Difference b/w map, filter and reduce
+# map → transforms, filter → selects, reduce → aggregates (combines into one result).
+
+#2 Difference b/w higher order function, closure and decorator.
+# Higher-order function → takes/returns functions, closure → remembers outer variables, decorator → wraps & modifies a function’s behavior.
+
+#3
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+for country in countries:
+    print(country)
+
+#4
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+for name in names:
+    print(name)
+
+#5
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in numbers:
+    print(number)
+
+
+# Exercise Level 2
