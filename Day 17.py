83803b9598dd64710d36dacb6e4351c2fad54557
Rohit Tiwari <rohit.tiwari968@gmail.com>
2026-04-15 21:27:24 +0530

    Complete Day 17
    Day 17 all exercise completed

--- Day 17.py
+++ Day 17.py
@@ -1,1 +1,6 @@
 # DAY 17: 30 Days of python programming
+
+#1
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_countries, es, ru = names
+print('Nordic Countries are: ', nordic_countries)
