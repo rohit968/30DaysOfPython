6946d8795ec4cf3416550fa133e27ca5cfb3a6f0
Rohit Tiwari <rohit.tiwari968@gmail.com>
2026-05-11 23:03:23 +0530

    Complete Day 22
    All Exercises of Day 22 completed

--- Day 22.py
+++ Day 22.py
@@ -1,2 +1,12 @@
 # DAY 22: 30 Days of python programming
 
+#1
+import requests
+from bs4 import BeautifulSoup
+import json
+
+URL = "http://www.bu.edu/president/boston-university-facts-stats/"
+
+# Send request
+response = requests.get(URL)
+print(response.text)
